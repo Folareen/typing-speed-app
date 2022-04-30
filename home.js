90fc28969569b53e8a1f1c396f35552ca190a3bd
Folareen <sakawahab03@gmail.com>
2022-04-30 21:33:54 +0100

    added getQuote function

--- home.js
+++ home.js
@@ -1,1 +1,24 @@
 // add speed func too
+
+const START_BUTTON = document.querySelector('.start');
+const START_OVERLAY = document.querySelector('.start-overlay');
+
+const QUOTE = document.querySelector('.quote');
+const TEXTAREA = document.querySelector('textarea');
+const TIMER = document.querySelector('.timer');
+
+START_BUTTON.addEventListener('click', startGame);
+
+function startGame(){
+    START_OVERLAY.style = "display:none";
+
+    getQuote();
+}
+
+
+async function getQuote(){
+    const res = await fetch('https://api.quotable.io/random');
+    const data = await res.json();
+
+    QUOTE.innerText = data.content;
+}
